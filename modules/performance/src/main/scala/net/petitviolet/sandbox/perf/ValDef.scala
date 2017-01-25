e3f7598632c44774befa71ed447fdc2c54aaaf52
petitviolet <violethero0820@gmail.com>
2017-01-26 08:00:46 +0900

    update val vs def

--- modules/performance/src/main/scala/net/petitviolet/sandbox/perf/ValDef.scala
+++ modules/performance/src/main/scala/net/petitviolet/sandbox/perf/ValDef.scala
@@ -3,13 +3,23 @@ package net.petitviolet.sandbox.perf
 import org.openjdk.jmh.annotations._
 
 /**
- * valとdefでほぼ変わらず
- * [info] Benchmark      Mode  Cnt          Score          Error  Units
- * [info] ValDef._def   thrpt  200  411956332.616 ±  6515261.167  ops/s
- * [info] ValDef._def2  thrpt  200  420121145.175 ±  1775055.484  ops/s
- * [info] ValDef._val   thrpt  200  417445058.704 ±  2016498.024  ops/s
- * [info] ValDef._val2  thrpt  200  391308496.556 ± 18399874.385  ops/s
- * sbt 'project performance' 'jmh:run -i 20 -wi 20  .*ValDef.*'
+ * a little `def` is faster
+ * [info] Benchmark         (N)   Mode  Cnt          Score          Error  Units
+ * [info] ValDef.defHeavy    10  thrpt   20    2407523.197 ±   106449.521  ops/s
+ * [info] ValDef.valHeavy    10  thrpt   20    2500756.190 ±    35970.424  ops/s
+ * [info] ValDef.defLight    10  thrpt   20  404379759.031 ±  4221763.228  ops/s
+ * [info] ValDef.valLight    10  thrpt   20  345622658.732 ±  2851731.608  ops/s
+ *
+ * [info] ValDef.defHeavy   100  thrpt   20     466049.253 ±     4312.192  ops/s
+ * [info] ValDef.valHeavy   100  thrpt   20     448293.509 ±     2568.814  ops/s
+ * [info] ValDef.defLight   100  thrpt   20  405220625.556 ±  2604729.646  ops/s
+ * [info] ValDef.valLight   100  thrpt   20  347918434.761 ±  2766661.683  ops/s
+ *
+ * [info] ValDef.defHeavy  1000  thrpt   20      48376.713 ±      834.762  ops/s
+ * [info] ValDef.valHeavy  1000  thrpt   20      46069.093 ±      416.818  ops/s
+ * [info] ValDef.defLight  1000  thrpt   20  361894876.917 ± 77044466.386  ops/s
+ * [info] ValDef.valLight  1000  thrpt   20  347141304.545 ±  2012639.189  ops/s
+ * sbt 'project performance' 'jmh:run -i 20 -wi 20 -f1 .*ValDef.*'
  */
 @State(Scope.Benchmark)
 class ValDef {
